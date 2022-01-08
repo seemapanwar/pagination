51f625ff6abb75aa7e2309627b8bb62794c4e030
seemapanwar <87643309+seemapanwar@users.noreply.github.com>
2022-01-09 00:45:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,559 @@
+const list_items = [
+	JSON.stringify({
+		id: '1',
+		name: 'Lura Senger',
+		email: 'Xander_Collier@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '2',
+		name: 'Wilburn Weber',
+		email: 'Bennett_Kreiger11@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '3',
+		name: 'Tyrique Hahn',
+		email: 'Juston.Altenwerth@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '4',
+		name: 'Lenny Bailey',
+		email: 'Guiseppe_Hegmann@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '5',
+		name: 'Vladimir Keeling',
+		email: 'Louisa_Walsh18@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '6',
+		name: 'Kellie Crona',
+		email: 'Chandler_Abernathy@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '7',
+		name: 'Carolina White',
+		email: 'Royal50@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '8',
+		name: 'Alfredo Conn',
+		email: 'Clarabelle34@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '9',
+		name: 'Stan Shanahan',
+		email: 'Lamar.McClure@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '10',
+		name: 'Marvin Fay',
+		email: 'Osbaldo58@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '11',
+		name: 'Torrance Rau',
+		email: 'Orin45@gmail.com',
+	}),
+	JSON.stringify({
+		id: '12',
+		name: 'Harold Gutmann MD',
+		email: 'Alyce.Stracke37@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '13',
+		name: 'Taryn Torphy',
+		email: 'Dean_Breitenberg71@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '14',
+		name: 'Bryana Lang',
+		email: 'Tatum.Ullrich@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '15',
+		name: 'Nyasia Green DDS',
+		email: 'Dino83@gmail.com',
+	}),
+	JSON.stringify({
+		id: '16',
+		name: 'Nasir Gerhold',
+		email: 'Lilian_Bashirian8@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '17',
+		name: 'Raymundo Ritchie PhD',
+		email: 'Antwan.Schoen15@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '18',
+		name: 'Delmer Marvin',
+		email: 'Kiera62@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '19',
+		name: 'Rachel Wilkinson',
+		email: 'Foster_Conroy@gmail.com',
+	}),
+	JSON.stringify({
+		id: '20',
+		name: 'Gladys Howell',
+		email: 'Constance.Labadie10@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '21',
+		name: 'Ciara Klocko',
+		email: 'Harvey76@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '22',
+		name: "Quentin O'Kon",
+		email: 'Amely_Cummings2@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '23',
+		name: 'Ms. Gabriella Kunde',
+		email: 'Lorenza_Cummerata@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '24',
+		name: 'Gerald Reilly',
+		email: 'Lia_Konopelski@gmail.com',
+	}),
+	JSON.stringify({
+		id: '25',
+		name: 'Brody Carter I',
+		email: 'Colten.Wilderman90@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '26',
+		name: 'Alanis Klocko',
+		email: 'Johathan.Champlin69@gmail.com',
+	}),
+	JSON.stringify({
+		id: '27',
+		name: 'Caroline Miller',
+		email: 'Delaney.Kozey74@gmail.com',
+	}),
+	JSON.stringify({
+		id: '28',
+		name: 'Ms. Merritt McClure',
+		email: 'Wendy.Zieme@gmail.com',
+	}),
+	JSON.stringify({
+		id: '29',
+		name: 'Jovani Schoen',
+		email: 'Norval_Zieme@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '30',
+		name: 'Berniece Bradtke',
+		email: 'Okey.Fisher39@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '31',
+		name: 'Hanna Gottlieb',
+		email: 'Edna62@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '32',
+		name: 'Christiana Bergnaum',
+		email: 'Donnie.Paucek22@gmail.com',
+	}),
+	JSON.stringify({
+		id: '33',
+		name: 'Reid Stehr',
+		email: 'Hiram69@gmail.com',
+	}),
+	JSON.stringify({
+		id: '34',
+		name: 'Evans Ward',
+		email: 'Liliane72@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '35',
+		name: 'Miss Enos Davis',
+		email: 'Kolby.Grady@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '36',
+		name: 'Mathew Lueilwitz',
+		email: 'Mariah.Hagenes@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '37',
+		name: 'Romaine Robel',
+		email: 'Sheila19@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '38',
+		name: 'Immanuel Aufderhar',
+		email: 'Aileen32@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '39',
+		name: 'Julius Reilly Jr.',
+		email: 'Santa89@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '40',
+		name: 'Linnea Borer',
+		email: 'Jarrett_Hegmann2@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '41',
+		name: 'Misael Abernathy IV',
+		email: 'Robbie42@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '42',
+		name: 'Mr. Gerda Jerde',
+		email: 'Gustave36@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '43',
+		name: 'Easton Daugherty',
+		email: 'Malcolm.Schuster11@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '44',
+		name: 'Leanna Gutkowski',
+		email: 'Chelsea9@gmail.com',
+	}),
+	JSON.stringify({
+		id: '45',
+		name: 'Rollin Trantow',
+		email: 'Marques_Hickle26@gmail.com',
+	}),
+	JSON.stringify({
+		id: '46',
+		name: 'Heaven Luettgen',
+		email: 'Lonnie.Greenfelder95@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '47',
+		name: 'Gilda Huel',
+		email: 'Stephania.Hintz@gmail.com',
+	}),
+	JSON.stringify({
+		id: '48',
+		name: 'Javon Walker',
+		email: 'Lamar_Bailey39@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '49',
+		name: 'Stewart King',
+		email: 'Chasity_Jenkins82@gmail.com',
+	}),
+	JSON.stringify({
+		id: '50',
+		name: 'Cortez Lueilwitz',
+		email: 'Lelia.Mitchell43@gmail.com',
+	}),
+	JSON.stringify({
+		id: '51',
+		name: 'Ora Kilback',
+		email: 'Matt25@gmail.com',
+	}),
+	JSON.stringify({
+		id: '52',
+		name: 'Lera Macejkovic',
+		email: 'Jamal.Donnelly45@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '53',
+		name: 'Asa Gottlieb',
+		email: 'Carolyne.Langosh@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '54',
+		name: 'Avery Nienow',
+		email: 'Hudson_Bernhard47@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '55',
+		name: 'Dr. Katrine Ryan',
+		email: 'Colten40@gmail.com',
+	}),
+	JSON.stringify({
+		id: '56',
+		name: 'Florida Kulas',
+		email: 'Alejandra_Bernhard@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '57',
+		name: 'Mauricio Gerlach',
+		email: 'Vincenza_Gusikowski84@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '58',
+		name: 'Chadrick Simonis',
+		email: 'Reymundo.Doyle@gmail.com',
+	}),
+	JSON.stringify({
+		id: '59',
+		name: 'Beau Jenkins',
+		email: 'Cletus65@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '60',
+		name: 'Camilla Cole',
+		email: 'Eda48@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '61',
+		name: 'Abagail MacGyver',
+		email: 'Jevon.Koch@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '62',
+		name: 'Ludwig Prohaska',
+		email: 'Eldon_DuBuque37@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '63',
+		name: 'Berenice Hane',
+		email: 'Lonnie3@gmail.com',
+	}),
+	JSON.stringify({
+		id: '64',
+		name: 'Abel Durgan',
+		email: 'Marlee.Weimann42@gmail.com',
+	}),
+	JSON.stringify({
+		id: '65',
+		name: 'Tate Kuhlman',
+		email: 'Hope31@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '66',
+		name: 'Mrs. Hermina Daniel',
+		email: 'Murphy59@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '67',
+		name: 'Marcelino Nienow',
+		email: 'Estefania_Dibbert97@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '68',
+		name: 'Raphaelle Connelly',
+		email: 'Josie.Legros70@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '69',
+		name: 'Mina Heaney',
+		email: 'Pete.Hermann79@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '70',
+		name: 'Jaida Torp',
+		email: 'Jonatan82@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '71',
+		name: 'Megane Fadel',
+		email: 'Dandre82@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '72',
+		name: 'Angelo Schimmel V',
+		email: 'Frederic54@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '73',
+		name: 'Jaren Ferry DDS',
+		email: 'Kristian.Kilback@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '74',
+		name: "Annette O'Connell",
+		email: 'Domenica.Christiansen@gmail.com',
+	}),
+	JSON.stringify({
+		id: '75',
+		name: 'Elenor Rohan PhD',
+		email: 'Alvera.Huel48@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '76',
+		name: 'Linnie Beier',
+		email: 'Reece_Berge@gmail.com',
+	}),
+	JSON.stringify({
+		id: '77',
+		name: 'Maybelle Lehner',
+		email: 'Arlo.Hagenes@gmail.com',
+	}),
+	JSON.stringify({
+		id: '78',
+		name: 'Hailie Haag',
+		email: 'Edythe.Lind59@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '79',
+		name: 'Marcelina Schuppe',
+		email: 'Kip.Cummerata77@gmail.com',
+	}),
+	JSON.stringify({
+		id: '80',
+		name: 'Ezra Lang',
+		email: 'Garret.Swaniawski@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '81',
+		name: 'Carey Kiehn',
+		email: 'Sabrina70@gmail.com',
+	}),
+	JSON.stringify({
+		id: '82',
+		name: 'Carole Aufderhar',
+		email: 'Efrain.Schamberger77@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '83',
+		name: 'Fanny Beatty',
+		email: 'Ewald28@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '84',
+		name: 'Mina Lubowitz',
+		email: 'Vincenzo.Herman@gmail.com',
+	}),
+	JSON.stringify({
+		id: '85',
+		name: 'Anabelle Thiel',
+		email: 'Amely.Renner@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '86',
+		name: 'Gerry Homenick',
+		email: 'Ada_Von54@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '87',
+		name: 'Domenico Maggio',
+		email: 'Freeman.Stamm@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '88',
+		name: 'Mauricio Bradtke III',
+		email: 'Alford83@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '89',
+		name: 'Bulah Yost III',
+		email: 'Nicolas_Schimmel6@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '90',
+		name: 'Orville Dibbert DDS',
+		email: 'Bryana_Herzog@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '91',
+		name: 'Autumn Osinski',
+		email: 'Davin8@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '92',
+		name: 'Levi Gibson',
+		email: 'Deangelo69@gmail.com',
+	}),
+	JSON.stringify({
+		id: '93',
+		name: 'Reva McKenzie',
+		email: 'Bret40@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '94',
+		name: 'Jensen Pfannerstill',
+		email: 'Dax_Marquardt@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '95',
+		name: 'Tyrese Larson MD',
+		email: 'Cecelia85@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '96',
+		name: 'Dayne Sanford',
+		email: 'Tyrique_Waters16@hotmail.com',
+	}),
+	JSON.stringify({
+		id: '97',
+		name: 'Jeramy Steuber',
+		email: 'Catalina.Stehr24@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '98',
+		name: 'Madge Bogisich',
+		email: 'Joey.Marquardt50@gmail.com',
+	}),
+	JSON.stringify({
+		id: '99',
+		name: 'Casimer MacGyver',
+		email: 'Julius.Bednar96@yahoo.com',
+	}),
+	JSON.stringify({
+		id: '100',
+		name: 'Arlene Kassulke',
+		email: 'Josiane_Smith@yahoo.com',
+	}),
+];
+
+const list_element = document.getElementById('list');
+const pagination_element = document.getElementById('pagination');
+
+let current_page = 1;
+let rows = 5;
+
+function DisplayList(items, wrapper, rows_per_page, page) {
+	wrapper.innerHTML = '';
+	page--;
+
+	let start = rows_per_page * page;
+	let end = start + rows_per_page;
+	let paginatedItems = items.slice(start, end);
+
+	for (let i = 0; i < paginatedItems.length; i++) {
+		let item = paginatedItems[i];
+
+		let item_element = document.createElement('div');
+		item_element.classList.add('item');
+		item_element.innerText = item;
+
+		wrapper.appendChild(item_element);
+	}
+}
+
+function SetupPagination(items, wrapper, rows_per_page) {
+	wrapper.innerHTML = '';
+
+	let page_count = Math.ceil(items.length / rows_per_page);
+	for (let i = 1; i < page_count + 1; i++) {
+		let btn = PaginationButton(i, items);
+		wrapper.appendChild(btn);
+	}
+}
+
+function PaginationButton(page, items) {
+	let button = document.createElement('button');
+	button.innerText = page;
+
+	if (current_page == page) button.classList.add('active');
+
+	button.addEventListener('click', function () {
+		current_page = page;
+		DisplayList(items, list_element, rows, current_page);
+
+		let current_btn = document.querySelector('.pagenumbers button.active');
+		current_btn.classList.remove('active');
+
+		button.classList.add('active');
+	});
+
+	return button;
+}
+
+DisplayList(list_items, list_element, rows, current_page);
+SetupPagination(list_items, pagination_element, rows);
